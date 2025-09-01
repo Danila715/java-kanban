fdaee6d083980a00691f8d34af3ca9f9e373fd6f
Jarvis <jarvis424@gmail.com>
2025-09-01 17:15:12 +0300

    Внесены изменения CodeStyle

--- src/main/manager/HistoryManager.java
+++ src/main/manager/HistoryManager.java
@@ -1,10 +1,13 @@
 package main.manager;
 
 import main.model.*;
+
 import java.util.*;
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
--- src/main/manager/TaskManager.java
+++ src/main/manager/TaskManager.java
@@ -1,27 +1,47 @@
 package main.manager;
 
 import main.model.*;
+
 import java.util.*;
 
 public interface TaskManager {
     Task createTask(String title, String description, TaskStatus status);
+
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubTasks();
+
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     SubTask getSubTaskById(int id);
+
     void addEpic(String title, String description);
+
     void addSubTask(String title, String description, int epicId, TaskStatus status);
+
     List<SubTask> getSubTasks(int epicId);
+
     void updateTask(Task updatedTask);
+
     void updateEpic(Epic updatedEpic);
+
     void updateSubTask(SubTask updatedSubTask);
+
     void deleteTaskById(int id);
+
     void deleteEpic(int id);
+
     void deleteSubTask(int id);
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<SubTask> getAllSubTasks();
+
     List<Task> getHistory();
 }
